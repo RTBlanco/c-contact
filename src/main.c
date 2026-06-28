1ae7a9b9bfea34be97bd20125e4f33c6e28ce955
RTBlanco <ronnytoribio1@hotmail.com>
2026-06-28 16:02:35 -0400

    made it possible to navigate actions

--- src/main.c
+++ src/main.c
@@ -13,11 +13,11 @@ Save contacts to a file
 Load contacts when the program starts
 */
 
-int add_contact();
-void display_contacts();
+int add_contact(int run);
+void show_all(int *run);
 void display_actions();
 void display_contact_actions();
-int search_contact();
+int search_contact(int run);
 int destroy_contact();
 int init_setup();
 
@@ -28,17 +28,52 @@ int main(int argc, char *argv[]) {
     init_setup();
     return 0;
   } else {
-    printf("No argument\n");
-  } 
+    
+    int running = 0;
+    int *run = &running;
+    char user_input[2];
 
-  int running = 0;
-  char user_input[2];
+    while (running == 0) {
+      display_actions();
+      fgets(user_input, sizeof(user_input), stdin);
 
-  while (running == 0) {
-    display_actions();
-    fgets(user_input, sizeof(user_input), stdin);
+      switch (user_input[0]) {
+        case '1':
+          show_all(run);
+          break;
+        case '2':
+          add_contact(*run);
+          break;
+        case '3':
+          search_contact(*run);
+          break;
+        default:
+          break;
+      }
+    }
+    
+    return 0;
   }
-  
+}
+
+void show_all(int *run) {
+  *run = 1;
+  printf("showing all Contacts\n");
+}
+
+int add_contact(int run) {
+
+  run = 1;
+  printf("add new contact\n");
+
+  return 0;
+}
+
+int search_contact(int run){
+  run = 1;
+
+  printf("Searching contact \n");
+
   return 0;
 }
 
@@ -51,6 +86,8 @@ void display_actions() {
   printf("\n");
 }
 
+
+
 int init_setup() {
   printf("initializing\n");
 
@@ -62,7 +99,7 @@ int init_setup() {
     printf("folder exists already\n");
   }
 
-  FILE *f = fopen("./contact_data/contacts.json", "w");
+  FILE *f = fopen("./contact_data/contacts.csv", "w");
   if (f == NULL ) {return 1;}
   fclose(f);
 
