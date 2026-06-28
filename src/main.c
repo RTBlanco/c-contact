d8cf1ef86d0032735ad9cf98920f93d66d51854c
RTBlanco <ronnytoribio1@hotmail.com>
2026-06-28 11:58:22 -0400

    set function prototypes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+Add a contact
+View all contacts
+Search by name
+Delete a contact
+Save contacts to a file
+Load contacts when the program starts
+*/
+
+int add_contact();
+void display_contacts();
+int search_contact();
+int destroy_contact();
+
+
 int main() {
   return 0;
 }
