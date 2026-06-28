4b6efcda267d31a63322a0b418eb25bb1635e44a
RTBlanco <ronnytoribio1@hotmail.com>
2026-06-28 13:54:38 -0400

    Refactor Makefile and implement initialization logic in main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
 
 /*
 Add a contact
@@ -14,8 +17,30 @@ int add_contact();
 void display_contacts();
 int search_contact();
 int destroy_contact();
+int init_setup();
 
 
-int main() {
+int main(int argc, char *argv[]) {
+  
+  if (argc > 1 && strcmp(argv[1], "init") == 0) {
+    init_setup();
+  } else {
+    printf("No argument\n");
+  } 
+
+  
+  return 0;
+}
+
+int init_setup() {
+  printf("initializing\n");
+
+  int folder = mkdir("contact_data", 0777);
+
+  if (folder == 0) {
+    printf("Folder was created\n");
+  } else {
+    printf("folder exists already\n");
+  }
   return 0;
 }
